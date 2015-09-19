08a9065aa4c78a45cf04712a17d5c4b5c7eb5293
Lior Alterman <lioralterman@gmail.com>
2015-09-19 22:56:16 +0300

    Created Vocabulary project

---
+++ C/Vocabulary/Vocabulary/Source.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/* Main */
+int main(int argc, char * argv[]) {
+
+	printf("Hello Army!\n\n");
+
+	/* stop the program from exiting */
+	char c = getchar();
+}
